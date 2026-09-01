4548c48632458bab9b050863b564a15cb4eb064b
Lyncon224 <lynconoliveira9710@gmail.com>
2026-09-01 14:55:25 -0300

    Resolvendo conflitos finais no _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,34 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack  screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="welcomeScreen" options={{ title: "Boas Vindas" }} />
-        <Stack.Screen name="chooseCurse" options={{ headerShown: false }}  />
-</Stack>
+
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("../../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Bold": require("../../assets/fonts/Poppins-Bold.ttf"),
+  });
+
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="welcomeScreen" options={{ title: "Boas Vindas" }} />
+      {/* A tela que você adicionou na sua branch */}
+      <Stack.Screen name="chooseCurse" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
